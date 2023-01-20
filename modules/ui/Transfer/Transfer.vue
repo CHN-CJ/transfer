6486dd736bf75f82fc98135e3cedfd1383c10f67
CHN-CJ <2873183291@qq.com>
2023-01-20 22:14:32 +0800

    样式编写

--- modules/ui/Transfer/Transfer.vue
+++ modules/ui/Transfer/Transfer.vue
@@ -15,12 +15,17 @@
         <div class="transfer">
             <div class="box left-list">
                 <h1 class="list-title"> {{ leftTitle }}</h1>
+                <div>
+
+                </div>
             </div>
             <div class="box button-group">
-
+                <button>&lt;</button>
+                <button>&gt;</button>
             </div>
             <div class="box right-list">
-
+                <h1 class="list-title"> {{ rightTitle }}</h1>
+                <div></div>
             </div>
         </div>
     </div>
@@ -37,7 +42,7 @@
         targetIndex,
         setTargetIndex
     ] = useTargetIndex(0);
-    
+
     const {
         leftTitle
     } = useComputedData(props.data, targetIndex);
@@ -47,5 +52,54 @@
 </script>
 
 <style lang="scss">
+.transfer {
+    display: flex;
+    width: 360px;
+    height: 300px;
+    border: 1px solid #ddd;
 
+    .box {
+        width: 120px;
+        height: 100%;
+
+        .list-title {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 38px;
+            font-weight: normal;
+            font-size: 14px;
+            margin: 0;
+            color: #666;
+            border-bottom: 1px solid #ddd;
+            background-color: #efefef;
+        }
+
+        &.button-group {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-left: 1px solid #ddd;
+            border-right: 1px solid #ddd;
+
+            button {
+                border: none;
+                outline: none;
+                width: 38px;
+                height: 38px;
+                background-color: orange;
+                color: #fff;
+                border-radius: 5px;
+
+                &:disabled {
+                    opacity: .6;
+                }
+
+                &:nth-child(2) {
+                    margin-left: 10px;
+                }
+            }
+        }
+    }
+}
 </style>
