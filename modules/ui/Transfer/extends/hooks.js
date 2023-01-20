817fcddfe272d5d4fc14503df46a55232c786dad
CHN-CJ <2873183291@qq.com>
2023-01-20 21:44:01 +0800

    添加props和hooks

---
+++ modules/ui/Transfer/extends/hooks.js
@@ -0,0 +1,23 @@
+import { computed, ref } from "vue";
+
+export function useTargetIndex(initialIndex) {
+    const targetIndex = ref(initialIndex);
+
+    function setTargetIndex(newIndex) {
+        targetIndex.value = Number(newIndex);
+        console.log(targetIndex.value);
+    }
+
+    return [
+        targetIndex,
+        setTargetIndex
+    ]
+}
+
+export function useComputedData(data, targetIndex) {
+    const leftTitle = computed(() => data[targetIndex.value].title);
+
+    return {
+        leftTitle
+    }
+}
